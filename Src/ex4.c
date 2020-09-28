0dc392cf00991dc334a7044bc50f71590c713768
SystemOutPrintLeal <victor_lealsk8@hotmail.com>
2020-09-27 21:51:50 -0300

    add Header

--- Src/ex4.c
+++ Src/ex4.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+struct Element{
+	int ligacoes;
+	char nome[20], tel[10];
+	struct Element * prox;
+};
+
+typedef struct Element element;
+
+element * lista_inicia();
+void lista_liga(element * LISTA, char * nome);
+void lista_insere(element * LISTA, const char * nome, const char * numero, int qnt);
+void lista_remove(element * LISTA, char * nome);
+void lista_imprime(element * LISTA);
+element * atPos(element * LISTA, int index);
+void node_swipe(element * LISTA, element * nodeA, element * nodeB);
+int indexOf(element * LISTA, element * Node);
+void lista_ordena(element * LISTA);
