1bfdd0ae74f21aa2db606772ec58f699a55ada12
SystemOutPrintLeal <victor_lealsk8@hotmail.com>
2020-09-28 20:37:39 -0300

    add functions

--- Src/ex4.c
+++ Src/ex4.c
@@ -19,3 +19,26 @@ element * atPos(element * LISTA, int index);
 void node_swipe(element * LISTA, element * nodeA, element * nodeB);
 int indexOf(element * LISTA, element * Node);
 void lista_ordena(element * LISTA);
+
+//INICIA UM NÓ APONTANDO PARA NULL
+element * lista_inicia(){
+	element * LISTA = (element*)malloc(sizeof(element));
+	if(!LISTA){
+		printf("Não alocada!\n");
+		exit(1);
+	}
+	LISTA->prox = NULL;
+	return LISTA;
+}
+
+//AUMENTA UM NAS LIGACOES DO NÓ EM QUE HÁ O NOME
+void lista_liga(element * LISTA, char * nome){
+	element * percorre = LISTA->prox;
+	while(percorre!=NULL){
+		if(strcmp(percorre->nome, nome) == 0){
+			percorre->ligacoes++;
+			break;
+		}
+		percorre = percorre->prox;
+	}
+}
